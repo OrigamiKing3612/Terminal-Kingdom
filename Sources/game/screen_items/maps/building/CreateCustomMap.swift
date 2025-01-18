eb3e8cf63221cb7bf9de86ae739ffa417d7ea65d
OrigamiKing3612 <115757805+OrigamiKing3612@users.noreply.github.com>
2025-01-17 19:04:11 -0600

    Make the all sides work

--- Sources/game/screen_items/maps/building/CreateCustomMap.swift
+++ Sources/game/screen_items/maps/building/CreateCustomMap.swift
@@ -64,11 +64,11 @@ enum CreateCustomMap {
 		}
 
 		if buildingOnTheLeft == false {
-			validDoorPosition = .right
+			validDoorPosition = .left
 		}
 
 		if buildingOnTheRight == false {
-			validDoorPosition = .left
+			validDoorPosition = .right
 		}
 
 		guard buildingsNearby == 3, let doorPosition = validDoorPosition else {
@@ -83,155 +83,6 @@ enum CreateCustomMap {
 			return building.isPlacedByPlayer
 		}
 		return false
-	}
-
-	private struct CreateMap {
-		var grid: [[MapTile]]
-		var x: Int
-		var y: Int
-		let startX: Int
-		let startY: Int
-		let doorPosition: DoorPosition
-
-		init(grid: [[MapTile]], x: Int, y: Int, doorPosition: DoorPosition) {
-			self.grid = grid
-			self.x = x
-			self.y = y
-			startX = x
-			startY = y
-			self.doorPosition = doorPosition
-		}
-
-		private enum CheckDoorDirection {
-			case up, down, left, right
-		}
-
-		mutating func rightSide() -> BuildingPerimeter {
-			calculatePerimeter(startingDirection: .up)
-		}
-
-		mutating func leftSide() -> BuildingPerimeter {
-			calculatePerimeter(startingDirection: .down)
-		}
-
-		mutating func top() -> BuildingPerimeter {
-			calculatePerimeter(startingDirection: .left)
-		}
-
-		mutating func bottom() -> BuildingPerimeter {
-			calculatePerimeter(startingDirection: .right)
-		}
-
-		private mutating func calculatePerimeter(startingDirection: CheckDoorDirection) -> BuildingPerimeter {
-			var hasReachedStart = false
-			var direction = startingDirection
-			var buildingPerimeter = BuildingPerimeter()
-
-			struct Position: Hashable {
-				var x: Int
-				var y: Int
-			}
-
-			var visited: Set<Position> = []
-			let startingPosition = Position(x: x, y: y)
-			visited.insert(startingPosition)
-
-			while !hasReachedStart {
-				// TODO: if moving the coordinates to 0,0 this might now work
-				switch direction {
-					case .right:
-						x += 1
-						if isWhereDoorWillGo(at: x, y: y) {
-							hasReachedStart = (x == startX && y == startY)
-							buildingPerimeter.rightSide += 1
-						} else if isBuilding(at: x, y: y) {
-							buildingPerimeter.rightSide += 1
-						} else {
-							direction = .up
-							x -= 1 // Backtrack to original position
-						}
-					case .up:
-						y -= 1
-						if isWhereDoorWillGo(at: x, y: y) {
-							hasReachedStart = (x == startX && y == startY)
-							buildingPerimeter.top += 1
-						} else if isBuilding(at: x, y: y) {
-							buildingPerimeter.top += 1
-						} else {
-							direction = .left
-							y += 1 // Backtrack to original position
-						}
-					case .left:
-						x -= 1
-						if isWhereDoorWillGo(at: x, y: y) {
-							hasReachedStart = (x == startX && y == startY)
-							buildingPerimeter.leftSide += 1
-						} else if isBuilding(at: x, y: y) {
-							buildingPerimeter.leftSide += 1
-						} else {
-							direction = .down
-							x += 1 // Backtrack to original position
-						}
-					case .down:
-						y += 1
-						if isWhereDoorWillGo(at: x, y: y) {
-							hasReachedStart = (x == startX && y == startY)
-							buildingPerimeter.bottom += 1
-						} else if isBuilding(at: x, y: y) {
-							buildingPerimeter.bottom += 1
-						} else {
-							direction = .right
-							y -= 1 // Backtrack to original position
-						}
-				}
-
-				// this = the door position
-
-				let currentPosition = Position(x: x, y: y)
-				visited.insert(currentPosition)
-
-				if currentPosition == startingPosition, visited.count > 1 {
-					hasReachedStart = true
-				}
-			}
-			var newBuildingPerimeter: BuildingPerimeter = .init()
-			switch doorPosition {
-				case .right:
-					newBuildingPerimeter.top = buildingPerimeter.top
-					newBuildingPerimeter.bottom = buildingPerimeter.bottom
-					newBuildingPerimeter.rightSide = buildingPerimeter.rightSide
-					newBuildingPerimeter.leftSide = buildingPerimeter.leftSide
-				case .top:
-					newBuildingPerimeter.top = buildingPerimeter.top + 1 + buildingPerimeter.bottom
-					newBuildingPerimeter.bottom = buildingPerimeter.top + 1 + buildingPerimeter.bottom
-					newBuildingPerimeter.rightSide = buildingPerimeter.rightSide - 1
-					newBuildingPerimeter.leftSide = buildingPerimeter.leftSide - 1
-				case .left:
-					newBuildingPerimeter.top = buildingPerimeter.top
-					newBuildingPerimeter.bottom = buildingPerimeter.bottom
-					newBuildingPerimeter.rightSide = buildingPerimeter.rightSide
-					newBuildingPerimeter.leftSide = buildingPerimeter.leftSide
-				case .bottom:
-					newBuildingPerimeter.top = buildingPerimeter.top + 1 + buildingPerimeter.bottom
-					newBuildingPerimeter.bottom = buildingPerimeter.top + 1 + buildingPerimeter.bottom
-					newBuildingPerimeter.rightSide = buildingPerimeter.rightSide - 1
-					newBuildingPerimeter.leftSide = buildingPerimeter.leftSide - 1
-			}
-
-			return newBuildingPerimeter
-		}
-
-		private func isBuilding(at x: Int, y: Int) -> Bool {
-			guard grid.indices.contains(y), grid[y].indices.contains(x) else { return false }
-			if case let .building(tile: building) = grid[y][x].type {
-				return building.isPlacedByPlayer
-			}
-			return false
-		}
-
-		private func isWhereDoorWillGo(at x: Int, y: Int) -> Bool {
-			x == startX && y == startY
-		}
 	}
 }
 
