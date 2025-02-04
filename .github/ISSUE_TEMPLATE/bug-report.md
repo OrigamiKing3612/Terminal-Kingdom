ee43dd66f6f4b4c96c1de29184b055b3b9aa3b5e
OrigamiKing3612 <115757805+OrigamiKing3612@users.noreply.github.com>
2025-02-04 15:04:20 -0600

    Windows Support (#17)
    * Initial work
    
    * Read my code
    
    (Lots of testing commits)
    
    * I think it works!
    
    * I think it will work!
    
    * Update workflows
    
    * Update issue template
    
    * Fix a bug I should do on master

--- .github/ISSUE_TEMPLATE/bug-report.md
+++ .github/ISSUE_TEMPLATE/bug-report.md
@@ -24,8 +24,9 @@ A clear and concise description of what you expected to happen.
 If applicable, add screenshots to help explain your problem.
 
 **Desktop (please complete the following information):**
- - OS: [e.g. Ubuntu or MacOS]
+ - OS: [e.g. Ubuntu, MacOS, Windows]
  - Version of OS
+ - Adventure Version
 
 **Additional context**
 Add any other context about the problem here.
--- Sources/game/Screen.swift
+++ Sources/game/Screen.swift
@@ -3,6 +3,8 @@ import Foundation
 	import Darwin
 #elseif os(Linux)
 	import Glibc
+#elseif os(Windows)
+	import WinSDK
 #endif
 
 enum Placement {
@@ -49,14 +51,22 @@ enum Screen {
 	}
 
 	private static func getTerminalSize() -> TerminalSize? {
-		var windowSize = winsize()
 		#if os(Linux)
+			var windowSize = winsize()
 			if ioctl(STDOUT_FILENO, UInt(TIOCGWINSZ), &windowSize) == 0 {
 				let rows = Int(windowSize.ws_row)
 				let columns = Int(windowSize.ws_col)
 				return TerminalSize(rows: rows, columns: columns)
 			}
+		#elseif os(Windows)
+			var consoleInfo = CONSOLE_SCREEN_BUFFER_INFO()
+			let handle = GetStdHandle(STD_OUTPUT_HANDLE)
+			GetConsoleScreenBufferInfo(handle, &consoleInfo)
+			let rows = Int(consoleInfo.srWindow.Bottom - consoleInfo.srWindow.Top + 1)
+			let columns = Int(consoleInfo.srWindow.Right - consoleInfo.srWindow.Left + 1)
+			return TerminalSize(rows: rows, columns: columns)
 		#else
+			var windowSize = winsize()
 			if ioctl(STDOUT_FILENO, TIOCGWINSZ, &windowSize) == 0 {
 				let rows = Int(windowSize.ws_row)
 				let columns = Int(windowSize.ws_col)
