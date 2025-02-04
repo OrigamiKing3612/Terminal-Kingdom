37c4bac2dcb8a37c0258e7aa52ba15056cc9f999
OrigamiKing3612 <115757805+OrigamiKing3612@users.noreply.github.com>
2025-02-04 12:56:35 -0600

    testing

--- Sources/game/input/os/WindowsTerminalInput.swift
+++ Sources/game/input/os/WindowsTerminalInput.swift
@@ -4,15 +4,21 @@ import Foundation
 #endif
 
 #if os(Windows)
+
 	enum WindowsTerminalInput {
 		static func readKey() -> KeyboardKeys {
 			var inputRecord = INPUT_RECORD()
 			var eventsRead: DWORD = 0
 			let hStdin = GetStdHandle(STD_INPUT_HANDLE)
 
+			// Ensure the handle is valid
+			guard hStdin != INVALID_HANDLE_VALUE else {
+				return .unknown
+			}
+
 			repeat {
 				ReadConsoleInputW(hStdin, &inputRecord, 1, &eventsRead)
-			} while inputRecord.EventType != KEY_EVENT || inputRecord.Event.KeyEvent.bKeyDown == FALSE
+			} while eventsRead == 0 || inputRecord.EventType != KEY_EVENT || inputRecord.Event.KeyEvent.bKeyDown == FALSE
 
 			let vkCode = inputRecord.Event.KeyEvent.wVirtualKeyCode
 			switch vkCode {
@@ -20,16 +26,23 @@ import Foundation
 				case 0x08: return .backspace
 				case 0x0D: return .enter
 				case 0x20: return .space
-				case 0x09: return .tab
+				case 0x09:
+					// Check if shift is held down for back-tab
+					if GetAsyncKeyState(VK_SHIFT) & 0x8000 != 0 {
+						return .back_tab
+					}
+					return .tab
 				case 0x2F: return .forward_slash
 				case 0xBF: return .questionMark
-				case 0x31 ... 0x39: return KeyboardKeys(rawValue: String(UnicodeScalar(vkCode))) ?? .unknown
-				case 0x41 ... 0x5A, 0x61 ... 0x7A: return KeyboardKeys(rawValue: String(UnicodeScalar(vkCode))) ?? .unknown
-				case VK_LEFT: return .left
-				case VK_RIGHT: return .right
-				case VK_UP: return .up
-				case VK_DOWN: return .down
-				case VK_TAB where GetKeyState(VK_SHIFT) & 0x8000 != 0: return .back_tab
+				case 0x30 ... 0x39, 0x41 ... 0x5A:
+					if let scalar = UnicodeScalar(vkCode) {
+						return KeyboardKeys(rawValue: String(scalar)) ?? .unknown
+					}
+				case 0x25: return .left
+				case 0x27: return .right
+				case 0x26: return .up
+				case 0x28: return .down
+				// Uncomment these lines if F-keys are needed
 				// case VK_F1: return .f1
 				// case VK_F2: return .f2
 				// case VK_F3: return .f3
@@ -44,6 +57,9 @@ import Foundation
 				// case VK_F12: return .f12
 				default: return .unknown
 			}
+
+			return .unknown
 		}
 	}
+
 #endif
