b3d1dc4690d33d004351800ba04040166fe6d4dc
OrigamiKing3612 <115757805+OrigamiKing3612@users.noreply.github.com>
2025-03-18 21:41:27 -0500

    Add a help popup

--- Sources/game/screen_items/TitleScreen.swift
+++ Sources/game/screen_items/TitleScreen.swift
@@ -13,42 +13,42 @@ struct TitleScreen {
 
 	mutating func show() async -> TitleScreenOptions {
 		// Quickly start a new game in debug mode
-		#if DEBUG
-			return TitleScreenOptions.newGameOption
-		#else
-			selectedOptionIndex = 0
-			while true {
-				Screen.clear()
-				let text = "Welcome to Terminal Kingdom!"
-				let x = middleX - (text.count / 2)
-				let y = middleY - (Screen.rows / 2)
-				Screen.print(x: x, y: y, text.styled(with: .bold))
-				let optionsX = middleX - (text.count / 4)
-				let icon = await " \(Game.shared.config.icons.selectedIcon)"
+		// #if DEBUG
+		// 	return TitleScreenOptions.newGameOption
+		// #else
+		selectedOptionIndex = 0
+		while true {
+			Screen.clear()
+			let text = "Welcome to Terminal Kingdom!"
+			let x = middleX - (text.count / 2)
+			let y = middleY - (Screen.rows / 2)
+			Screen.print(x: x, y: y, text.styled(with: .bold))
+			let optionsX = middleX - (text.count / 4)
+			let icon = await " \(Game.shared.config.icons.selectedIcon)"
 
-				for (index, option) in TitleScreenOptions.allCases.enumerated() {
-					let isSelected = selectedOptionIndex == index
-					Screen.print(x: optionsX, y: y + 3 + index, "\(isSelected ? icon : "  ")\(option.label)".styled(with: .bold, styledIf: isSelected))
-				}
-
-				let key = TerminalInput.readKey()
-				switch key {
-					case .enter:
-						return TitleScreenOptions.allCases[selectedOptionIndex]
-					case .up, .w, .k, .back_tab:
-						selectedOptionIndex = max(0, selectedOptionIndex - 1)
-					case .down, .s, .j, .tab:
-						selectedOptionIndex = min(TitleScreenOptions.allCases.count - 1, selectedOptionIndex + 1)
-					case .zero:
-						Screen.clear()
-						Screen.initialize()
-					case .q:
-						endProgram()
-					default:
-						break
-				}
+			for (index, option) in TitleScreenOptions.allCases.enumerated() {
+				let isSelected = selectedOptionIndex == index
+				Screen.print(x: optionsX, y: y + 3 + index, "\(isSelected ? icon : "  ")\(option.label)".styled(with: .bold, styledIf: isSelected))
 			}
-		#endif
+
+			let key = TerminalInput.readKey()
+			switch key {
+				case .enter:
+					return TitleScreenOptions.allCases[selectedOptionIndex]
+				case .up, .w, .k, .back_tab:
+					selectedOptionIndex = max(0, selectedOptionIndex - 1)
+				case .down, .s, .j, .tab:
+					selectedOptionIndex = min(TitleScreenOptions.allCases.count - 1, selectedOptionIndex + 1)
+				case .zero:
+					Screen.clear()
+					Screen.initialize()
+				case .q:
+					endProgram()
+				default:
+					break
+			}
+		}
+		// #endif
 	}
 
 	enum TitleScreenOptions: CaseIterable {
@@ -87,7 +87,7 @@ struct TitleScreen {
 						await newGame()
 					}
 				case .helpOption:
-					screen.help()
+					return
 				case .settingsOption:
 					await screen.settings()
 				case .quitOption:
@@ -142,25 +142,6 @@ struct TitleScreen {
 		}
 	}
 
-	func help() {
-		let text = "Help"
-		let x = middleX
-		let y = middleY - (Screen.rows / 2)
-		Screen.print(x: x - (text.count / 2), y: y, text.styled(with: .bold))
-		var yStart = 3
-		// TODO: change depending on user's config
-		yStart = printHelpMessage(x: x, y: y + yStart, "Press \("wasd".styled(with: .bold)) or the \("arrow keys".styled(with: .bold)) to move.")
-		yStart = printHelpMessage(x: x, y: y + yStart, "Press \(KeyboardKeys.space.render) or \(KeyboardKeys.enter.render) to interact with the tile you are on.")
-		yStart = printHelpMessage(x: x, y: y + yStart, "Press \(KeyboardKeys.i.render) to open the inventory.")
-		yStart = printHelpMessage(x: x, y: y + yStart, "Press \(KeyboardKeys.b.render) to start building.")
-		yStart = printHelpMessage(x: x, y: y + yStart, "Press \(KeyboardKeys.zero.render) to resize the screen.")
-		yStart = printHelpMessage(x: x, y: y + yStart, "Press \(KeyboardKeys.W.render) and \(KeyboardKeys.S.render) to scroll up and down in the message box.")
-		yStart = printHelpMessage(x: x, y: y + yStart, "Press \(KeyboardKeys.Q.render) to quit.")
-
-		yStart = printHelpMessage(x: x, y: y + yStart, "Press any key to leave.")
-		Screen.print(x: Screen.columns - 1 - Game.version.count, y: Screen.rows - 1, Game.version)
-	}
-
 	mutating func settings() async {
 		// TODO: reload config after saving so this isn't async
 		config = await Config.load()
@@ -212,11 +193,6 @@ struct TitleScreen {
 		let isSelected = selectedSettingOptionIndex == index
 		let configOptionToPrint = configOption == "" ? "" : ": \(configOption)"
 		await Screen.print(x: x - (text.count / 2), y: y, "\(isSelected ? "\(Game.shared.config.icons.selectedIcon) " : " ")\(text)\(configOptionToPrint)".styled(with: .bold, styledIf: isSelected))
-		return y + 1
-	}
-
-	private func printHelpMessage(x: Int, y: Int, _ text: String) -> Int {
-		Screen.print(x: x - (text.withoutStyles.count / 2), y: y, text)
 		return y + 1
 	}
 }
