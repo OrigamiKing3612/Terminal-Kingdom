33f6809eea8e92cdec8c7c3c22071bdb489c7189
OrigamiKing3612 <115757805+OrigamiKing3612@users.noreply.github.com>
2025-03-21 14:46:26 -0500

    Add edit village event

--- Sources/game/event/events/EditKingdomEvent.swift
+++ Sources/game/event/events/EditKingdomEvent.swift
@@ -1,27 +1,24 @@
 import Foundation
 
 enum EditKingdomEvent {
-	static func editKingdom(villageID: UUID) async {
+	static func editKingdom() async {
 		// TODO: make this into a pop up
 		var exit = false
+		let kingdom = await Game.shared.kingdom
 		while !exit {
-			guard let village = await Game.shared.kingdom.get(villageID: villageID) else {
-				await MessageBox.message("An error has occurred", speaker: .game)
-				return
-			}
 			let options: [MessageOption] = [
 				.init(label: "Quit", action: { exit = true }),
-				.init(label: "Rename Kingdom", action: { await renameKingdom(village: village) }),
+				.init(label: "Rename Kingdom", action: { await renameKingdom() }),
 			]
-			await MessageBox.message("Editing \(village.name):", speaker: .game)
+			await MessageBox.message("Editing \(kingdom.name):", speaker: .game)
 			await MessageBox.messageWithOptions("What do you want to do?", speaker: .game, options: options).action()
 		}
 	}
 
-	static func renameKingdom(village: Village) async {
+	static func renameKingdom() async {
 		let name = await MessageBox.messageWithTyping("New Name:", speaker: .game)
 		if !name.isEmpty {
-			await Game.shared.kingdom.renameVillage(id: village.id, name: name)
+			await Game.shared.renameKingdom(newName: name)
 		}
 	}
 }
--- Sources/game/event/events/door_events/ShopDoorEvent.swift
+++ Sources/game/event/events/door_events/ShopDoorEvent.swift
@@ -1,11 +1,8 @@
-enum ShopDoorEvent {
+enum ShopDoorEvent: DoorEvent {
 	static func open(tile: DoorTile) async {
 		var options: [MessageOption] = [
 			.init(label: "Go Inside", action: { await goInside(tile: tile) }),
 		]
-		if tile.isPartOfPlayerVillage {
-			options.append(.init(label: "Upgrade", action: { upgrade(tile: tile) }))
-		}
 		options.append(.init(label: "Quit", action: {}))
 		let selectedOption = await MessageBox.messageWithOptions("What would you like to do?", speaker: .game, options: options)
 		await selectedOption.action()
@@ -13,9 +10,5 @@ enum ShopDoorEvent {
 
 	static func goInside(tile _: DoorTile) async {
 		await MapBox.setMapType(.shop)
-	}
-
-	static func upgrade(tile _: DoorTile) {
-		// TODO: upgrade building
 	}
 }
