692f05221bf2be012893b3eda66b70ea0f062d83
OrigamiKing3612 <115757805+OrigamiKing3612@users.noreply.github.com>
2025-03-25 11:53:12 -0500

    Try windows shift again

--- Sources/game/input/os/WindowsTerminalInput.swift
+++ Sources/game/input/os/WindowsTerminalInput.swift
@@ -26,7 +26,7 @@ enum WindowsTerminalInput {
 				case 13: return .enter
 				case 32: return .space
 				case 9:
-					if (UInt16(GetAsyncKeyState(VK_SHIFT)) & 0x8000) != 0 {
+					if (UInt16(GetAsyncKeyState(0x10)) & 0x8000) != 0 {
 						Logger.debug("Shift + Tab")
 						return .back_tab
 					}
@@ -36,15 +36,15 @@ enum WindowsTerminalInput {
 				case 0x2F: return .forward_slash
 				case 0xBF: return .questionMark
 				case 0x30 ... 0x39, 0x41 ... 0x5A:
-					if let scalar = UnicodeScalar(vkCode) {
-						Logger.debug("Pressed letter key")
-						if (UInt16(GetAsyncKeyState(VK_SHIFT)) & 0x8000) != 0 {
-							Logger.debug("Is pressing shift")
-							return KeyboardKeys(rawValue: String(scalar).uppercased()) ?? .unknown
-						} else {
-							Logger.debug("not pressing shift \(UInt16(GetAsyncKeyState(VK_SHIFT)))")
-							return KeyboardKeys(rawValue: String(scalar).lowercased()) ?? .unknown
-						}
+					if let scalar = mapVirtualKeyToCharacter(vkCode: vkCode) {
+						// Logger.debug("Pressed letter key")
+						// if (UInt16(GetAsyncKeyState(0x10)) & 0x8000) != 0 {
+						// 	Logger.debug("Is pressing shift")
+						return KeyboardKeys(rawValue: scalar) ?? .unknown
+						// } else {
+						// 	Logger.debug("not pressing shift \(UInt16(GetAsyncKeyState(0x10)))")
+						// 	return KeyboardKeys(rawValue: scalar.lowercased()) ?? .unknown
+						// }
 					}
 				case 0x25: return .left
 				case 0x27: return .right
@@ -55,6 +55,48 @@ enum WindowsTerminalInput {
 
 			Logger.warning("Unknown key: \(vkCode)")
 			return .unknown
+		}
+
+		private static func mapVirtualKeyToCharacter(vkCode: UInt16) -> String? {
+			switch vkCode {
+				case 0x30: "0"
+				case 0x31: "1"
+				case 0x32: "2"
+				case 0x33: "3"
+				case 0x34: "4"
+				case 0x35: "5"
+				case 0x36: "6"
+				case 0x37: "7"
+				case 0x38: "8"
+				case 0x39: "9"
+				case 0x41: "A"
+				case 0x42: "B"
+				case 0x43: "C"
+				case 0x44: "D"
+				case 0x45: "E"
+				case 0x46: "F"
+				case 0x47: "G"
+				case 0x48: "H"
+				case 0x49: "I"
+				case 0x4A: "J"
+				case 0x4B: "K"
+				case 0x4C: "L"
+				case 0x4D: "M"
+				case 0x4E: "N"
+				case 0x4F: "O"
+				case 0x50: "P"
+				case 0x51: "Q"
+				case 0x52: "R"
+				case 0x53: "S"
+				case 0x54: "T"
+				case 0x55: "U"
+				case 0x56: "V"
+				case 0x57: "W"
+				case 0x58: "X"
+				case 0x59: "Y"
+				case 0x5A: "Z"
+				default: nil
+			}
 		}
 	#endif
 }
