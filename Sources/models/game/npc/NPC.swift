dab4a77ee7ebed3533037a675f1806ae6fe213be
OrigamiKing3612 <115757805+OrigamiKing3612@users.noreply.github.com>
2025-03-25 13:16:35 -0500

    Add more NPC names

--- Sources/models/game/npc/NPC.swift
+++ Sources/models/game/npc/NPC.swift
@@ -7,7 +7,7 @@ struct NPC: Codable, Hashable, Equatable {
 	var hasTalkedToBefore: Bool
 	var job: NPCJob?
 	// skill level
-	// let age: Int
+	let age: Int
 	let gender: Gender
 	let villageID: UUID?
 	private(set) var positionToWalkTo: TilePosition?
@@ -15,7 +15,7 @@ struct NPC: Codable, Hashable, Equatable {
 	private var _hunger: Double = 100
 	private var hunger: Hunger { Hunger.hunger(for: _hunger) }
 
-	init(id: UUID = UUID(), name: String? = nil, gender: Gender? = nil, job: NPCJob? = nil, isStartingVillageNPC: Bool = false, positionToWalkTo: TilePosition? = nil, tilePosition: NPCPosition? = nil, villageID: UUID) {
+	init(id: UUID = UUID(), name: String? = nil, gender: Gender? = nil, job: NPCJob? = nil, isStartingVillageNPC: Bool = false, positionToWalkTo: TilePosition? = nil, tilePosition: NPCPosition? = nil, villageID: UUID, age: Int = Int.random(in: 18 ... 40)) {
 		self.id = id
 		self.gender = gender ?? Gender.allCases.randomElement()!
 		self.name = name ?? Self.generateRandomName(for: self.gender)
@@ -32,6 +32,7 @@ struct NPC: Codable, Hashable, Equatable {
 			await Game.shared.kingdom.add(npcID: id, villageID: villageID)
 		}
 		self.villageID = villageID
+		self.age = age
 	}
 
 	mutating func tick() async {
@@ -71,6 +72,11 @@ struct NPC: Codable, Hashable, Equatable {
 			"Kyle", "Leo", "Matt", "Nate", "Owen",
 			"Paul", "Quinn", "Ryan", "Sam", "Tom",
 			"Victor", "Theo", "Jim", "Eli", "Mark",
+			"John", "David", "Michael", "James", "Robert",
+			"Alexander", "Fred", "Garrett", "Charles", "John",
+			"William", "Henry", "George", "Edward", "Thomas",
+			"Joseph", "Frank", "Patrick", "Peter", "Simon",
+			"Nathaniel", "Ethan", "Luke", "Josh", "Liam",
 		]
 
 		let femaleNames = [
@@ -79,6 +85,11 @@ struct NPC: Codable, Hashable, Equatable {
 			"Kate", "Lily", "Mia", "Nora", "Olivia",
 			"Paige", "Jordan", "Rachel", "Sarah", "Tessa",
 			"Violet", "Wendy", "Zoey", "Ellie", "Claire",
+			"Emily", "Haley", "Isabel", "Katie", "Lila",
+			"Maggie", "Natalie", "Olivia", "Piper", "Hannah",
+			"Rebecca", "Sophie", "Tara", "Ursula", "Vivian",
+			"Caroline", "Alice", "Lily", "Taylor", "Megan",
+			"Ava", "Sophia", "Amelia", "Harper", "Evelyn",
 		]
 
 		return (gender == .male ? maleNames : femaleNames).randomElement()!
@@ -164,6 +175,52 @@ struct NPC: Codable, Hashable, Equatable {
 				}
 			}
 		}
+	}
+
+	private enum CodingKeys: CodingKey {
+		case id
+		case name
+		case isStartingVillageNPC
+		case hasTalkedToBefore
+		case job
+		case age
+		case gender
+		case villageID
+		case positionToWalkTo
+		case attributes
+		case hunger
+	}
+
+	init(from decoder: any Decoder) throws {
+		let container: KeyedDecodingContainer<NPC.CodingKeys> = try decoder.container(keyedBy: NPC.CodingKeys.self)
+
+		self.id = try container.decode(UUID.self, forKey: NPC.CodingKeys.id)
+		self.name = try container.decode(String.self, forKey: NPC.CodingKeys.name)
+		self.isStartingVillageNPC = try container.decode(Bool.self, forKey: NPC.CodingKeys.isStartingVillageNPC)
+		self.hasTalkedToBefore = try container.decode(Bool.self, forKey: NPC.CodingKeys.hasTalkedToBefore)
+		self.job = try container.decodeIfPresent(NPCJob.self, forKey: NPC.CodingKeys.job)
+		self.age = try container.decode(Int.self, forKey: NPC.CodingKeys.age)
+		self.gender = try container.decode(Gender.self, forKey: NPC.CodingKeys.gender)
+		self.villageID = try container.decodeIfPresent(UUID.self, forKey: NPC.CodingKeys.villageID)
+		self.positionToWalkTo = try container.decodeIfPresent(TilePosition.self, forKey: NPC.CodingKeys.positionToWalkTo)
+		self.attributes = try container.decode([NPCAttribute].self, forKey: NPC.CodingKeys.attributes)
+		self._hunger = try container.decode(Double.self, forKey: NPC.CodingKeys.hunger)
+	}
+
+	func encode(to encoder: any Encoder) throws {
+		var container: KeyedEncodingContainer<NPC.CodingKeys> = encoder.container(keyedBy: NPC.CodingKeys.self)
+
+		try container.encode(id, forKey: NPC.CodingKeys.id)
+		try container.encode(name, forKey: NPC.CodingKeys.name)
+		try container.encode(isStartingVillageNPC, forKey: NPC.CodingKeys.isStartingVillageNPC)
+		try container.encode(hasTalkedToBefore, forKey: NPC.CodingKeys.hasTalkedToBefore)
+		try container.encodeIfPresent(job, forKey: NPC.CodingKeys.job)
+		try container.encode(age, forKey: NPC.CodingKeys.age)
+		try container.encode(gender, forKey: NPC.CodingKeys.gender)
+		try container.encodeIfPresent(villageID, forKey: NPC.CodingKeys.villageID)
+		try container.encodeIfPresent(positionToWalkTo, forKey: NPC.CodingKeys.positionToWalkTo)
+		try container.encode(attributes, forKey: NPC.CodingKeys.attributes)
+		try container.encode(_hunger, forKey: NPC.CodingKeys.hunger)
 	}
 }
 
