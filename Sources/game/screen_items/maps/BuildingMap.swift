322480a8551d0e41df4ea529f15a6c2936db0408
OrigamiKing3612 <115757805+OrigamiKing3612@users.noreply.github.com>
2025-04-02 13:46:22 -0500

    Add the helpers

--- Sources/game/screen_items/maps/BuildingMap.swift
+++ Sources/game/screen_items/maps/BuildingMap.swift
@@ -198,37 +198,58 @@ struct BuildingMap: MapBoxMap {
 			}
 		}
 		let npcsInManager = await Game.shared.npcs.npcs
+		var index = 1
 		for npc in npcs {
 			if !npcsInManager.keys.contains(npc) {
-				let job: NPCJob? = switch mapType {
+				var job: NPCJob? = nil
+				switch mapType {
 					case .blacksmith:
-						.blacksmith
+						job = .blacksmith
 					case .builder:
-						.builder
+						if index == 1 || index == 2 {
+							job = .builder_helper
+						} else {
+							job = .builder
+						}
 					case .carpenter:
-						.carpenter
+						if index == 3 {
+							job = .carpenter
+						} else {
+							job = .carpenter_helper
+						}
 					case .castle:
-						.king
+						job = .king
 					case .farm:
-						.farmer
+						if index == 1 {
+							index += 1
+							job = .farmer
+						} else {
+							job = .farmer_helper
+						}
 					case .hospital:
-						.doctor
+						job = .doctor
 					case .hunting_area:
-						.hunter
+						job = .hunter
 					case .inventor:
-						.inventor
+						job = .inventor
 					case .mine:
-						.miner
+						job = .miner
 					case .potter:
-						.potter
+						job = .potter
 					case .restaurant:
-						.chef
+						job = .chef
 					case .shop:
-						.salesman(type: .buy)
+						if index == 1 {
+							job = .salesman(type: .buy)
+						} else if index == 2 {
+							job = .salesman(type: .sell)
+						} else {
+							job = .salesman(type: .help)
+						}
 					case .stable:
-						.stable_master
+						job = .stable_master
 					default:
-						nil
+						job = nil
 				}
 				await Game.shared.npcs.add(npc: NPC(id: npc, job: job, isStartingVillageNPC: true, villageID: UUID(), position: .init(x: 0, y: 0, mapType: mapType)))
 			}
