246076664dac994cf03992501e8d56b5da1c03f1
OrigamiKing3612 <115757805+OrigamiKing3612@users.noreply.github.com>
2025-05-03 15:30:33 -0500

    Clean up moving function

--- Sources/models/game/npc/NPCMoving.swift
+++ Sources/models/game/npc/NPCMoving.swift
@@ -3,127 +3,137 @@ import Foundation
 extension NPCTile {
 	static func move(position: NPCMovingPosition) async {
 		// Logger.debug("\(#function) \(#file):\(#line): Moving NPC from \(position.x), \(position.y)")
-		let npcTile = await MapBox.mapType.map.grid[position.y][position.x] as! MapTile
-		if case let .npc(tile: tile) = npcTile.type, let positionToWalkTo = await tile.npc.positionToWalkTo {
+		let tileNPCisOn = await MapBox.mapType.map.grid[position.y][position.x] as! MapTile
+		if case let .npc(tile: tile) = tileNPCisOn.type, let positionToWalkTo = await tile.npc.positionToWalkTo {
 			// Reached the destination
-			if positionToWalkTo == .init(x: position.x, y: position.y, mapType: position.mapType) {
-				await Game.shared.removeNPC(position)
-				if case let .door(doorTile) = position.oldTile.type {
-					if case let .custom(mapID, _) = doorTile.type {
-						// TODO: fix this when multidoors in custom maps
-						guard let mapID else { return }
-						guard let grid = await Game.shared.maps.customMaps[mapID]?.grid else { return }
-
-						for (yIndex, y) in grid.enumerated() {
-							if let xIndex = y.firstIndex(where: { if case .door = $0.type { true } else { false }}) {
-								// Restore the door tile
-								await MapBox.updateTile(newTile: MapTile(
-									type: .door(tile: doorTile),
-									isWalkable: npcTile.isWalkable,
-									event: npcTile.event,
-									biome: npcTile.biome
-								), x: position.x, y: position.y)
-								var doorPosition: DoorPosition = .bottom
-								if yIndex == 0 {
-									doorPosition = .top
-								} else if yIndex == grid.count - 1 {
-									doorPosition = .bottom
-								} else if xIndex == 0 {
-									doorPosition = .left
-								} else if xIndex == grid[yIndex].count - 1 {
-									doorPosition = .right
-								}
-								let npcX: Int
-								let npcY: Int
-								switch doorPosition {
-									case .left:
-										npcX = xIndex + 1
-										npcY = yIndex + 1
-									case .right:
-										npcX = xIndex - 1
-										npcY = yIndex - 1
-									case .top:
-										npcX = xIndex - 1
-										npcY = yIndex + 1
-									case .bottom:
-										npcX = xIndex + 1
-										npcY = yIndex - 1
-								}
-
-								var newGrid = grid
-								await Game.shared.npcs.removeNPCPosition(npcID: tile.npcID)
-								newGrid[npcY][npcX] = MapTile(
-									type: .npc(tile: .init(id: tile.id, npcID: tile.npcID)),
-									isWalkable: npcTile.isWalkable,
-									event: npcTile.event,
-									biome: npcTile.biome
-								)
-								await Game.shared.maps.updateCustomMap(id: mapID, with: newGrid)
-								break
-							}
-						}
-
-					} else {}
-					// put npcID in the custom map
-					return
-				} else {
-					await Game.shared.npcs.removeNPCPosition(npcID: tile.npcID)
-					await MapBox.updateTile(newTile: MapTile(
-						type: .npc(tile: .init(id: tile.id, npcID: tile.npcID)),
-						isWalkable: npcTile.isWalkable,
-						event: npcTile.event,
-						biome: npcTile.biome
-					), x: position.x, y: position.y)
-					return
-				}
+			if hasReachedDestination(position, target: positionToWalkTo) {
+				await reachedDestination(position: position, tileNPCisOn: tileNPCisOn, tile: tile)
 			}
 			let newNpcPosition = await NPCMoving.move(
 				target: positionToWalkTo,
 				current: .init(x: position.x, y: position.y, mapType: position.mapType)
 			)
 			guard await MapBox.mapType == position.mapType else { return }
-			let currentTile = await MapBox.mapType.map.grid[newNpcPosition.y][newNpcPosition.x] as! MapTile
+			await updateMapForNPCMove(from: position, to: newNpcPosition, tile: tile, npcTile: tileNPCisOn)
+		}
+	}
 
-			let newPosition = NPCMovingPosition(
-				x: newNpcPosition.x,
-				y: newNpcPosition.y,
-				mapType: position.mapType,
-				oldTile: currentTile
-			)
+	private static func reachedDestination(position: NPCMovingPosition, tileNPCisOn: MapTile, tile: NPCTile) async {
+		await Game.shared.removeNPC(position)
+		if case let .door(doorTile) = position.oldTile.type {
+			if case let .custom(mapID, _) = doorTile.type {
+				// TODO: fix this when multidoors in custom maps
+				guard let mapID else { return }
+				guard let grid = await Game.shared.maps.customMaps[mapID]?.grid else { return }
 
-			await withTaskGroup { group in
-				group.addTask {
-					// Restore old tile
-					await MapBox.setMapGridTile(
-						x: position.x,
-						y: position.y,
-						tile: position.oldTile,
-						mapType: position.mapType
-					)
+				for (yIndex, y) in grid.enumerated() {
+					if let xIndex = y.firstIndex(where: { if case .door = $0.type { true } else { false }}) {
+						// Restore the door tile
+						await MapBox.updateTile(newTile: MapTile(
+							type: .door(tile: doorTile),
+							isWalkable: tileNPCisOn.isWalkable,
+							event: tileNPCisOn.event,
+							biome: tileNPCisOn.biome
+						), x: position.x, y: position.y)
+						var doorPosition: DoorPosition = .bottom
+						if yIndex == 0 {
+							doorPosition = .top
+						} else if yIndex == grid.count - 1 {
+							doorPosition = .bottom
+						} else if xIndex == 0 {
+							doorPosition = .left
+						} else if xIndex == grid[yIndex].count - 1 {
+							doorPosition = .right
+						}
+						let npcX: Int
+						let npcY: Int
+						switch doorPosition {
+							case .left:
+								npcX = xIndex + 1
+								npcY = yIndex + 1
+							case .right:
+								npcX = xIndex - 1
+								npcY = yIndex - 1
+							case .top:
+								npcX = xIndex - 1
+								npcY = yIndex + 1
+							case .bottom:
+								npcX = xIndex + 1
+								npcY = yIndex - 1
+						}
+
+						var newGrid = grid
+						await Game.shared.npcs.removeNPCPosition(npcID: tile.npcID)
+						newGrid[npcY][npcX] = MapTile(
+							type: .npc(tile: .init(id: tile.id, npcID: tile.npcID)),
+							isWalkable: tileNPCisOn.isWalkable,
+							event: tileNPCisOn.event,
+							biome: tileNPCisOn.biome
+						)
+						await Game.shared.maps.updateCustomMap(id: mapID, with: newGrid)
+						break
+					}
 				}
 
-				group.addTask {
-					// Update new tile to NPC
-					await MapBox.setMapGridTile(
-						x: newNpcPosition.x,
-						y: newNpcPosition.y,
-						tile: MapTile(
-							type: .npc(tile: tile),
-							isWalkable: npcTile.isWalkable,
-							event: npcTile.event,
-							biome: npcTile.biome
-						),
-						mapType: position.mapType
-					)
-				}
+			} else {}
+			// put npcID in the custom map
+			return
+		} else {
+			await Game.shared.npcs.removeNPCPosition(npcID: tile.npcID)
+			await MapBox.updateTile(newTile: MapTile(
+				type: .npc(tile: .init(id: tile.id, npcID: tile.npcID)),
+				isWalkable: tileNPCisOn.isWalkable,
+				event: tileNPCisOn.event,
+				biome: tileNPCisOn.biome
+			), x: position.x, y: position.y)
+			return
+		}
+	}
 
-				group.addTask {
-					await Game.shared.updateNPC(oldPosition: position, newPosition: newPosition)
-				}
+	private static func hasReachedDestination(_ position: NPCMovingPosition, target: TilePosition) -> Bool {
+		target == .init(x: position.x, y: position.y, mapType: position.mapType)
+	}
 
-				group.addTask {
-					await MapBox.updateTwoTiles(x1: newNpcPosition.x, y1: newNpcPosition.y, x2: position.x, y2: position.y)
-				}
+	private static func updateMapForNPCMove(from position: NPCMovingPosition, to newPos: NPCMoving.Position, tile: NPCTile, npcTile: MapTile) async {
+		let currentTile = await MapBox.mapType.map.grid[newPos.y][newPos.x] as! MapTile
+
+		let newPosition = NPCMovingPosition(
+			x: newPos.x,
+			y: newPos.y,
+			mapType: position.mapType,
+			oldTile: currentTile
+		)
+
+		await withTaskGroup { group in
+			group.addTask {
+				await MapBox.setMapGridTile(
+					x: position.x,
+					y: position.y,
+					tile: position.oldTile,
+					mapType: position.mapType
+				)
+			}
+			group.addTask {
+				await MapBox.setMapGridTile(
+					x: newPos.x,
+					y: newPos.y,
+					tile: MapTile(
+						type: .npc(tile: tile),
+						isWalkable: npcTile.isWalkable,
+						event: npcTile.event,
+						biome: npcTile.biome
+					),
+					mapType: position.mapType
+				)
+			}
+			group.addTask {
+				await Game.shared.updateNPC(oldPosition: position, newPosition: newPosition)
+			}
+			group.addTask {
+				await MapBox.updateTwoTiles(
+					x1: newPos.x, y1: newPos.y,
+					x2: position.x, y2: position.y
+				)
 			}
 		}
 	}
