9c451c520bc0d2df5f70b4905c83739b70b8cdca
OrigamiKing3612 <115757805+OrigamiKing3612@users.noreply.github.com>
2025-05-06 11:34:16 -0500

    Stuff

--- Sources/models/game/npc/jobs/work/BlackmsithWork.swift
+++ Sources/models/game/npc/jobs/work/BlackmsithWork.swift
@@ -1,5 +1,15 @@
 enum LeadBlacksmithWork {
 	static func work(npc: inout NPC) async {
+		guard case let .lead_blacksmith(leadBlacksmithTasks) = npc.task else {
+			return
+		}
+		switch leadBlacksmithTasks {
+			case let .collectMaterials(items, from):
+			case let .create(item):
+			case .idle:
+			case .moveTo:
+			case let .smelt(item):
+		}
 		//! TODO: Implement LeadBlacksmithWork
 	}
 }
